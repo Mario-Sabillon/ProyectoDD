1d8f240d9ab736aeff77f8590fae986755c192e5
Mario-Sabillon <mario.sabilln@unah.hn>
2025-04-06 14:47:53 -0600

    Agrego Middleware de Autenticacion

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,18 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const authenticate = (req, res, next) => {
+const token = req.header('Authorization');
+
+if (!token) return res.status(403).json({ message: 'Acceso denegado' });
+
+try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+} catch (err) {
+    return res.status(401).json({ message: 'Token inválido' });
+}
+};
